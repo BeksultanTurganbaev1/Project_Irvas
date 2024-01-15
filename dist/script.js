6ebb251023bb2998c8e672db2d055c1cb1770c61
Beksultan Turganbaev <beksultan.turganbaev.kk@gmail.com>
2024-01-15 16:09:26 +0300

    add amimation and last commit

--- dist/script.js
+++ dist/script.js
@@ -215,7 +215,8 @@ const modals = state => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll('[data-modal]');
+      windows = document.querySelectorAll('[data-modal]'),
+      scroll = calcScroll();
     trigger.forEach(item => {
       const stopBtn = e => {
         if (e.target) {
@@ -226,6 +227,7 @@ const modals = state => {
         });
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
       };
       item.addEventListener('click', stopBtn);
     });
@@ -235,6 +237,7 @@ const modals = state => {
       });
       modal.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
     });
     modal.addEventListener('click', e => {
       if (e.target === modal && closeClickOverlay) {
@@ -243,6 +246,7 @@ const modals = state => {
         });
         modal.style.display = 'none';
         document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
       }
     });
   }
@@ -262,6 +266,17 @@ const modals = state => {
       bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
     }
   });
+  function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = (state) => {
         const trigger = document.querySelectorAll(triggerSelector),
               modal = document.querySelector(modalSelector),
               close = document.querySelector(closeSelector),
-              windows = document.querySelectorAll('[data-modal]');
+              windows = document.querySelectorAll('[data-modal]'),
+              scroll = calcScroll();
 
 
         trigger.forEach(item => {
@@ -19,6 +20,7 @@ const modals = (state) => {
 
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
             };
 
             item.addEventListener('click', stopBtn);
@@ -31,6 +33,7 @@ const modals = (state) => {
 
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.style.marginRight = `0px`;
         });
 
         modal.addEventListener('click', (e) => {
@@ -41,6 +44,7 @@ const modals = (state) => {
 
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
+                document.body.style.marginRight = `0px`;
             }
         });
     }
@@ -63,6 +67,21 @@ const modals = (state) => {
             bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
         }
     });
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
